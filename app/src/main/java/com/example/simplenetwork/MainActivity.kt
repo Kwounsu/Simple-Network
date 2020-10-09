a95cac8f26cae899617520b88c1d8ac48c70ea77
Kwounsu <sb00lgs@gmail.com>
2020-10-09 12:17:53 -0700

    DownloadCallback Interface to extend in the MainActivity

--- app/src/main/java/com/example/simplenetwork/MainActivity.kt
+++ app/src/main/java/com/example/simplenetwork/MainActivity.kt
@@ -2,8 +2,9 @@ package com.example.simplenetwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity(), DownloadCallback<String> {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
